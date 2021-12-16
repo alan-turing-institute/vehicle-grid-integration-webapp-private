579a3586af9b9d9ee08dbc11b42733f58119861f
Oscar Giles <ogiles@turing.ac.uk>
2021-12-16 14:03:16 +0000

    add options route

--- vgi_api/vgi_api/validation/types.py
+++ vgi_api/vgi_api/validation/types.py
@@ -10,6 +10,16 @@ class NetworkID(str, Enum):
     RURAL = "1061"
 
 
+class AllOptions(str, Enum):
+
+    MVSolarPVOptions = "mv-solar-pv"
+    MVEVChargerOptions = "mv-ev-charger"
+    LVSmartMeterOptions = "lv-smartmeter"
+    LVElectricVehicleOptions = "lv-ev"
+    LVPVOptions = "lv-pv"
+    LVHPOptions = "lv-hp"
+
+
 class DefaultLV(str, Enum):
 
     NEAR_SUB = "near-sub"
@@ -21,49 +31,37 @@ class MVSolarPVOptions(str, Enum):
 
     NONE = "None"
     OPTION1 = "Option a"
-    OPTION2 = "Option b"
-    OPTION3 = "Option c"
     CSV = "csv"
 
 
 class MVEVChargerOptions(str, Enum):
 
     NONE = "None"
-    OPTION1 = "Option d"
-    OPTION2 = "Option e"
-    OPTION3 = "Option f"
+    OPTION1 = "Option a"
     CSV = "csv"
 
 
 class LVSmartMeterOptions(str, Enum):
     NONE = "None"
-    OPTION1 = "Option g"
-    OPTION2 = "Option h"
-    OPTION3 = "Option i"
+    OPTION1 = "Option a"
     CSV = "csv"
 
 
 class LVElectricVehicleOptions(str, Enum):
     NONE = "None"
-    OPTION1 = "Option j"
-    OPTION2 = "Option k"
-    OPTION3 = "Option l"
+    OPTION1 = "Option a"
     CSV = "csv"
 
 
 class LVPVOptions(str, Enum):
     NONE = "None"
-    OPTION1 = "1"
-    OPTION2 = "2"
-    OPTION3 = "3"
+    OPTION1 = "a"
     CSV = "csv"
 
 
 class LVHPOptions(str, Enum):
     NONE = "None"
-    OPTION1 = "1"
-    OPTION2 = "2"
-    OPTION3 = "3"
+    OPTION1 = "a"
     CSV = "csv"
 
 
@@ -97,37 +95,25 @@ DATA_FOLDER = Path(__file__).parent.parent / "data"
 # ToDo: Get all profiles, stick in data folder and put file name here
 SOLAR_PROFILES: Dict[MVSolarPVOptions, Path] = {
     MVSolarPVOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    MVSolarPVOptions.OPTION2: DATA_FOLDER / "",
-    MVSolarPVOptions.OPTION3: DATA_FOLDER / "",
 }
 
 EV_PROFILES: Dict[MVEVChargerOptions, Path] = {
     MVEVChargerOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    MVEVChargerOptions.OPTION2: DATA_FOLDER / "",
-    MVEVChargerOptions.OPTION3: DATA_FOLDER / "",
 }
 
 SMART_METER_PROFILES: Dict[LVSmartMeterOptions, Path] = {
     LVSmartMeterOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    LVSmartMeterOptions.OPTION2: DATA_FOLDER / "",
-    LVSmartMeterOptions.OPTION3: DATA_FOLDER / "",
 }
 
 LV_EV_PROFILES: Dict[LVElectricVehicleOptions, Path] = {
     LVElectricVehicleOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    LVElectricVehicleOptions.OPTION2: DATA_FOLDER / "",
-    LVElectricVehicleOptions.OPTION3: DATA_FOLDER / "",
 }
 
 LV_PV_PROFILES: Dict[LVPVOptions, Path] = {
     LVPVOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    LVPVOptions.OPTION2: DATA_FOLDER / "",
-    LVPVOptions.OPTION3: DATA_FOLDER / "",
 }
 
 
 LV_HP_PROFILES: Dict[LVHPOptions, Path] = {
     LVHPOptions.OPTION1: DATA_FOLDER / "example_profile.csv",
-    LVHPOptions.OPTION2: DATA_FOLDER / "",
-    LVHPOptions.OPTION3: DATA_FOLDER / "",
 }
