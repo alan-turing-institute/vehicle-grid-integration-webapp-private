33bc75f9b311b1e20b9d9722b59c86d8f58d2832
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-01-12 18:03:02 +0000

    Remove deprecated panel

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,24 +1,5 @@
 <template>
   <div class="row">
-    <div class="card mr-2" style="width: 20rem;">
-      <img
-        src="https://images.unsplash.com/photo-1454779132693-e5cd0a216ed3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80"
-        class="card-img-top"
-        alt="unsplash-image"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Electricity distribution network</h5>
-        <p class="card-text">
-          Build and simulate an electricity distribution network, using OpenDSS.
-        </p>
-        <!--
-        <router-link class="btn btn-primary" :to="{ name: 'SimulateNetwork' }"
-          >Build a Network</router-link
-        >
-        -->
-        <p>Deprecated</p>
-      </div>
-    </div>
     <div class="card" style="width: 20rem;">
       <img
         src="https://images.unsplash.com/photo-1454779132693-e5cd0a216ed3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80"
