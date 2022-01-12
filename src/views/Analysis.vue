70f7f047d4d6c4ad4088b26c33a4897c46f4c1ab
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-01-12 19:37:43 +0000

    Add text to new landing page

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -1,22 +1,39 @@
 <template>
-  <div class="row">
-    <div class="card" style="width: 20rem;">
-      <img
-        src="https://images.unsplash.com/photo-1454779132693-e5cd0a216ed3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2251&q=80"
-        class="card-img-top"
-        alt="unsplash-image"
-      />
-      <div class="card-body">
-        <h5 class="card-title">Electricity distribution network API</h5>
-        <p class="card-text">
-          Build and simulate an electricity distribution network, using OpenDSS.
-        </p>
-        <router-link
-          class="btn btn-primary"
-          :to="{ name: 'SimulateNetworkAPI' }"
-          >Build a Network</router-link
-        >
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6" style="margin-bottom: 20px">
+        <div class="box-main" style="border-color: #3949AB; height: 100%">
+          <div class="col-lg-12 box-title" style="background-color: #C5CAE9">
+            <h3>EVENT</h3>
+          </div>
+          <div class="center-element">
+            <h5><u>E</u>lectric <u>VE</u>hicle <u>N</u>etwork analysis <u>T</u>ool</h5>
+          </div>
+          <div class="center-element">
+            <router-link
+              class="btn btn-primary"
+              style="background-color: #3949AB; border-color: #3949AB"
+              :to="{ name: 'SimulateNetworkAPI' }"
+              >Build a Network
+            </router-link>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6" style="margin-bottom: 20px">
+        <div class="box-main" style="border-color: #1E88E5; height: 100%">
+          <div class="col-lg-12 box-title" style="background-color: #BBDEFB">
+            <h3>Acknowledgements</h3>
+          </div>
+          <div class="col-lg-12">
+            <p>This work is a collaboration of Newcastle University, Research Engineering at The Alan Turing Institute and the Supergen Energy Networks Hub.</p>
+            <p>For more information see <a href="https://www.turing.ac.uk/research/research-projects/vehicle-grid-integration">the project website</a>.</p>
+            <p>This work is funded by the e4Future grant (104227) - under UK Research and Innovation by the Department for Business, Energy and Industrial Strategy (BEIS) and The Office for Zero Emission Vehicles (OZEV) and delivered through Innovate UK; and a grant from the Lloyd's Register Foundation (G0095) under the Data-Centric Engineering Programme of The Alan Turing Institute.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
 </template>
