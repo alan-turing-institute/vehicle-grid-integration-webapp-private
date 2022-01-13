0037608d1b2bbb3d8f5fd4c95dc9a32564a6c784
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-01-13 09:33:42 +0000

    Give each modal a unique ID

--- src/components/InputDetails.vue
+++ src/components/InputDetails.vue
@@ -1,10 +1,10 @@
 <template>
 
-    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#inputDetailsModal">
+    <button type="button" class="btn btn-link" data-toggle="modal" :data-target="'#inputDetailsModal'+idModal">
     <i class="bi bi-info-circle"></i>
     </button>
 
-    <div class="modal fade" id="inputDetailsModal" tabindex="-1">
+    <div class="modal fade" :id="'inputDetailsModal'+idModal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
@@ -25,10 +25,14 @@
 </template>
 
 <script>
+    import { v4 as uuidv4 } from "uuid";
     export default {
         props: ["inputName", "inputInfo", "inputValues"],
         data() {
-            return { showModal: false}
-        }
+            return { 
+                showModal: false,
+                idModal: uuidv4(),
+            }
+        },
     }
 </script>
