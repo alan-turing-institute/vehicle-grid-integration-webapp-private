a63f8612d49f5f7fa250e2be73ec0b2656527565
Louise Bowler <LouiseABowler@users.noreply.github.com>
2022-01-13 19:58:07 +0000

    Rearrange main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,26 +2,35 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-lg-6" style="margin-bottom: 20px">
+      <div class="col-lg-12" style="margin-bottom: 20px">
         <div class="box-main" style="border-color: #3949AB; height: 100%">
           <div class="col-lg-12 box-title" style="background-color: #C5CAE9">
             <h3>EVENT</h3>
           </div>
           <div class="center-element">
-            <h5><u>E</u>lectric <u>VE</u>hicle <u>N</u>etwork analysis <u>T</u>ool</h5>
+            <h1><u>E</u>lectric <u>VE</u>hicle <u>N</u>etwork analysis <u>T</u>ool</h1>
           </div>
           <div class="center-element">
+            <div class="col-lg-4 offset-4">
             <router-link
-              class="btn btn-primary"
-              style="background-color: #3949AB; border-color: #3949AB"
+              class="btn btn-primary btn-block"
+              style="background-color: #3949AB; border-color: #3949AB; font-size: 24px"
               :to="{ name: 'SimulateNetworkAPI' }"
               >Build a Network
             </router-link>
+            </div>
+          </div>
+          <div class="row" style="padding: 60px 20px 20px">
+            <img class="col-md-3 logo-main" src="../assets/logos/supergen.png">
+            <img class="col-md-3 logo-main" src="../assets/logos/turing.png">
+            <img class="col-md-3 logo-main" src="../assets/logos/newcastle.png">
+            <img class="col-md-3 logo-main" src="../assets/logos/lrf.svg">
           </div>
         </div>
       </div>
-
-      <div class="col-lg-6" style="margin-bottom: 20px">
+    </div>
+    <div class="row">
+      <div class="col-lg-12" style="margin-bottom: 20px">
         <div class="box-main" style="border-color: #1E88E5; height: 100%">
           <div class="col-lg-12 box-title" style="background-color: #BBDEFB">
             <h3>Acknowledgements</h3>
@@ -30,14 +39,6 @@
             <p>This work is a collaboration of Newcastle University, Research Engineering at The Alan Turing Institute and the Supergen Energy Networks Hub.</p>
             <p>For more information see <a href="https://www.turing.ac.uk/research/research-projects/vehicle-grid-integration">the project website</a>.</p>
             <p>This work is funded by the e4Future grant (104227) - under UK Research and Innovation by the Department for Business, Energy and Industrial Strategy (BEIS) and The Office for Zero Emission Vehicles (OZEV) and delivered through Innovate UK; and a grant from the Lloyd's Register Foundation (G0095) under the Data-Centric Engineering Programme of The Alan Turing Institute.</p>
-          </div>
-          <div class="row" style="padding: 10px 20px">
-            <img class="col-md-6 logo" src="../assets/logos/supergen.png">
-            <img class="col-md-6 logo" src="../assets/logos/turing.png">
-          </div>
-          <div class="row" style="padding: 10px 20px">
-            <img class="col-md-6 logo" src="../assets/logos/newcastle.png">
-            <img class="col-md-6 logo" src="../assets/logos/lrf.svg">
           </div>
         </div>
       </div>
