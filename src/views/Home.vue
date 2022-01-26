54b1eed11f83f975dd2dd72f420af77edb26f864
myriam-ncl <myriam.neaimeh@ncl.ac.uk>
2022-01-26 16:54:09 +0000

    home_changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
 
+
   <div class="container">
     <div class="row">
       <div class="col-lg-12" style="margin-bottom: 20px">
@@ -10,7 +11,14 @@
           <div class="center-element">
             <h1><u>E</u>lectric <u>VE</u>hicle <u>N</u>etwork analysis <u>T</u>ool</h1>
           </div>
+
           <div class="center-element">
+            <div class="img-fluid"><img src="../assets/logos/220126_icons_web.png" alt="Responsive image">
+            </div>
+          </div>
+          
+            <div class="center-element">
+            
             <div class="col-lg-4 offset-4">
             <router-link
               class="btn btn-primary btn-block"
@@ -26,7 +34,8 @@
             <img class="col-md-3 logo-main" src="../assets/logos/newcastle.png">
             <img class="col-md-3 logo-main" src="../assets/logos/lrf.svg">
           </div>
-        </div>
+
+        <!-- </div>
       </div>
     </div>
     <div class="row">
@@ -39,10 +48,16 @@
             <p>This work is a collaboration of Newcastle University, Research Engineering at The Alan Turing Institute and the Supergen Energy Networks Hub.</p>
             <p>For more information see <a href="https://www.turing.ac.uk/research/research-projects/vehicle-grid-integration">the project website</a>.</p>
             <p>This work is funded by the e4Future grant (104227) - under UK Research and Innovation by the Department for Business, Energy and Industrial Strategy (BEIS) and The Office for Zero Emission Vehicles (OZEV) and delivered through Innovate UK; and a grant from the Lloyd's Register Foundation (G0095) under the Data-Centric Engineering Programme of The Alan Turing Institute.</p>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
   </div>
+
+<footer class="footer mt-auto py-3">
+  <div class="container text-center">
+    <span class="text-muted">Copyright &copy; 2019-2022 <strong>e4futuregrid.com</strong>. All rights reserved.</span>
+  </div>
+</footer>
 
 </template>
