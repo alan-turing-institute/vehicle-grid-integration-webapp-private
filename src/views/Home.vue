aaca28c4c7e5d92736b0fbd87e8568d599b703c7
myriam-ncl <myriam.neaimeh@ncl.ac.uk>
2022-01-28 19:16:53 +0000

    resized for different screen sizes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,8 @@
           
             <div class="center-element">
             
-            <div class="col-lg-4 offset-4">
+            <!-- <div class="col-lg-4 offset-4"> -->
+            <div class="col-6 offset-3">
             <router-link
               class="btn btn-primary btn-block"
               style="background-color: #3949AB; border-color: #3949AB; font-size: 24px"
@@ -29,27 +30,14 @@
             </router-link>
             </div>
           </div>
-          <div class="row" style="padding: 60px 20px 20px">
-            <img class="col-md-3 logo-main" src="../assets/logos/supergen.png" alt="Responsive image">
-            <img class="col-md-3 logo-main" src="../assets/logos/turing.png" alt="Responsive image">
-            <img class="col-md-3 logo-main" src="../assets/logos/newcastle.png" alt="Responsive image">
-            <img class="col-md-3 logo-main" src="../assets/logos/lrf.svg" alt="Responsive image">
-          </div>
 
-        <!-- </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12" style="margin-bottom: 20px">
-        <div class="box-main" style="border-color: #1E88E5; height: 100%">
-          <div class="col-lg-12 box-title" style="background-color: #BBDEFB">
-            <h3>Acknowledgements</h3>
-          </div>
-          <div class="col-lg-12">
-            <p>This work is a collaboration of Newcastle University, Research Engineering at The Alan Turing Institute and the Supergen Energy Networks Hub.</p>
-            <p>For more information see <a href="https://www.turing.ac.uk/research/research-projects/vehicle-grid-integration">the project website</a>.</p>
-            <p>This work is funded by the e4Future grant (104227) - under UK Research and Innovation by the Department for Business, Energy and Industrial Strategy (BEIS) and The Office for Zero Emission Vehicles (OZEV) and delivered through Innovate UK; and a grant from the Lloyd's Register Foundation (G0095) under the Data-Centric Engineering Programme of The Alan Turing Institute.</p>
-          </div> -->
+          <div class="row">
+            <img class="col-md-3 logo" style="padding-top:20px" src="../assets/logos/supergen.png">
+            <img class="col-md-3 logo" style="padding-top:20px" src="../assets/logos/turing.png">
+            <img class="col-md-3 logo" style="padding-top:20px" src="../assets/logos/newcastle.png">
+            <img class="col-md-3 logo" style="padding-top:20px" src="../assets/logos/lrf.svg">
+          </div>          
+
         </div>
       </div>
     </div>
